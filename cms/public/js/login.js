ddd8163c9d81a0fa5d3eea701e2591d56702e9dd
Choppychan <s.carmen2101@gmail.com>
2020-01-27 09:50:09 +0100

    Add page of login and the js code for it

---
+++ cms/public/js/login.js
@@ -0,0 +1,16 @@
+
+axios
+  .post('http://localhost:1337/auth/local', {
+    identifier: 'user@strapi.io',
+    password: 'strapiPassword',
+  })
+  .then(response => {
+
+    console.log('Well done!');
+    console.log('User profile', response.data.user);
+    console.log('User token', response.data.jwt);
+  })
+  .catch(error => {
+
+    console.log('An error occurred:', error);
+  });
